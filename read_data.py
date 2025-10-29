f92eb2b8866932f67eb64608f8fcc3d086768771
Ian33 <ianrhiggins@gmail.com>
2025-10-29 13:12:32 -0700

    import data function

---
+++ read_data.py
@@ -0,0 +1,18 @@
+## data source  ##
+# https://www.sos.wa.gov/elections/data-research/election-results-and-voters-pamphlets/2025-primary-election
+# https://results.vote.wa.gov/results/20250805/snohomish/
+
+import pandas as pd
+
+file_path = "data/20250805_snohomishprecincts.csv"
+
+data = pd.read_csv(file_path, header = None)
+# rename columns
+data.columns = ["contest", "candidate", "municipality", "precinct", "votes"]
+data = data[data['precinct'] != -1]
+data = data[data["candidate"] != "WRITE-IN"]
+print(data)
+# list unique contests
+print(data["contest"].unique())
+# list unique municipalities
+print(data["municipality"].unique())
